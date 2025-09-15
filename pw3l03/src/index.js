a465f35061ae7ae52bb0d74b47c4eca9616967df
Vi <107654718+fifth-monger@users.noreply.github.com>
2025-09-15 16:42:04 -0500

    this is how my instructor did it and it looks cleaner and less lines of code

--- pw3l03/src/index.js
+++ pw3l03/src/index.js
@@ -1,14 +1,15 @@
 let country = "Guinea Bissau";
-country = country.replace(" ", "-");
-console.log(country);
+console.log(country.replace(" ", "-"));
+
 let city = " Sydney   ";
 city = city.trim();
 console.log(city);
-let place = "School";
-place = place.toUpperCase();
-console.log(place);
+
 let attraction = "Opera House";
-attraction = attraction.toLowerCase();
-console.log(attraction);
-let sentance = `I went to visit the ${attraction} in ${city}, right next to my school. Now, I'm getting ready for my new trip to ${country}!`;
-console.log(sentance);
+console.log(attraction.toLowerCase());
+
+let place = "School";
+console.log(place.toUpperCase());
+
+let sentence = `I went to visit the ${attraction} in ${city}, right next to my ${place.toLowerCase()}. Now, I'm getting ready for my trip to ${country}!`;
+console.log(sentence);
