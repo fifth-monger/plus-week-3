8866033a75151ed50f925edc96f2f72ac02db0b3
Vi <107654718+fifth-monger@users.noreply.github.com>
2025-09-16 17:01:04 -0500

    creating objects and arrays in JS

---
+++ pw3l06/src/index.js
@@ -0,0 +1,54 @@
+//challenge1
+let weather = {};
+console.log(weather);
+
+//challenge2
+weather.temp = 89;
+weather.humidity = 42;
+console.log(weather);
+
+//challenge3
+console.log(weather.temp);
+console.log(weather.humidity);
+
+//challenge4
+weather.windSpeed = 10;
+console.log(weather.windSpeed);
+
+//challenge5
+console.log(weather["windSpeed"]);
+console.log(weather["temp"]);
+console.log(weather["humidity"]);
+
+//challenge6
+let forecast = [
+  {
+    day: "Monday",
+    temp: 89,
+  },
+  {
+    day: "Tuesday",
+    temp: 88,
+  },
+  {
+    day: "Wednesday",
+    temp: 90,
+  },
+  {
+    day: "Thursday",
+    temp: 90,
+  },
+  {
+    day: "Friday",
+    temp: 91,
+  },
+  {
+    day: "Saturday",
+    temp: 92,
+  },
+  {
+    day: "Sunday",
+    temp: 91,
+  },
+];
+console.log(forecast);
