39d94297cbcc4ad001eadf751e8a10cf50b3fd4f
Vi <107654718+fifth-monger@users.noreply.github.com>
2025-09-16 17:53:46 -0500

    objects vs array challenge

---
+++ pw3l07/src/index.js
@@ -0,0 +1,53 @@
+let paris = {
+  name: "Paris",
+  country: "France",
+  language: "French",
+  temperature: 18,
+  capitalCity: true,
+};
+//challenge1
+console.log(paris.temperature);
+
+//challenge2
+let sydney = {
+  name: "Sydney",
+  country: "Australia",
+  language: "English",
+  temperature: 21,
+  capitalCity: false,
+};
+console.log(sydney.temperature);
+
+//challenge3
+let cityNames = ["Sydney", "Melborne", "Brisbane"];
+console.log(cityNames[0]);
+console.log(cityNames[1]);
+console.log(cityNames[2]);
+
+//challenge4
+let cities = [
+  {
+    name: "Melborne",
+    country: "Australia",
+    language: "English",
+    temperature: 12,
+    capitalCity: false,
+  },
+  {
+    name: "Mexico City",
+    country: "Mexico",
+    language: "Spanish",
+    temperature: 22,
+    capitalCity: true,
+  },
+  {
+    name: "Florence",
+    country: "Italy",
+    language: "Italian",
+    temperature: 19,
+    capitalCity: false,
+  },
+];
+console.log(cities[0].temperature);
+console.log(cities[1].temperature);
+console.log(cities[2].temperature);
